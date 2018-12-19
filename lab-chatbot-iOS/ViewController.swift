f7704dcebb7e7d24e066890808c37ea1bdb43398
Francesca <fharo@belatrixsf.com>
2018-12-19 09:39:48 -0500

    Get y Post

--- lab-chatbot-iOS/ViewController.swift
+++ lab-chatbot-iOS/ViewController.swift
@@ -12,9 +12,59 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
-
-
+ 
+    @IBAction func onGetTapped(_ sender: Any) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/users") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+                
+            }
+            }.resume()
+    }
+    
+    @IBAction func onPostTapped(_ sender: Any) {
+        
+        let parameters = ["username": "@kilo_loco", "tweet": "HelloWorld"]
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else { return }
+        request.httpBody = httpBody
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            if let response = response {
+                print(response)
+            }
+            
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print(error)
+                }
+            }
+            
+            }.resume()
+    }
+    
 }
 
